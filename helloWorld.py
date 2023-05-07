81111ac0643b22208c6a317f8941ca9ca906eeb3
Parveen Jethwa <127699288+JethwaParv@users.noreply.github.com>
2023-05-07 10:42:32 +0100

    Create helloWorld.py

---
+++ helloWorld.py
@@ -0,0 +1,8 @@
+# Compulsory Task 1
+# Assigned variable.
+# input statement allows the user to their name.
+
+name = input("What is your name?")
+age = input("What is your age?")
+print('My name is {} and i am {} years old'.format(name, age))
+print('Hello World')
